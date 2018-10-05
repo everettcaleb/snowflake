6874f9e6eb6079a3a007e668563c0f0047779823
Caleb Everett <everettcaleb95@gmail.com>
2018-10-05 18:50:14 -0400

    Fixed tag version

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 if [[ $TRAVIS_TAG ]]; then
-  TAG_W_MINOR=`echo -n $TRAVIS_TAG | sed 's/\.[0-9]*$//g'`
-  TAG_MAJOR=`echo -n $TRAVIS_TAG | sed 's/\.[0-9]*\.[0-9]*$//g'`
+  TAG_VERSION=`echo -n $TRAVIS_TAG | sed 's/v//'`
+  TAG_W_MINOR=`echo -n $TAG_VERSION | sed 's/\.[0-9]*$//g'`
+  TAG_MAJOR=`echo -n $TAG_VERSION | sed 's/\.[0-9]*\.[0-9]*$//g'`
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker tag everettcaleb/snowflake everettcaleb/snowflake:$TRAVIS_TAG
+  docker tag everettcaleb/snowflake everettcaleb/snowflake:$TAG_VERSION
   docker tag everettcaleb/snowflake everettcaleb/snowflake:$TAG_W_MINOR
   docker tag everettcaleb/snowflake everettcaleb/snowflake:$TAG_MAJOR
-  docker push everettcaleb/snowflake:$TRAVIS_TAG
+  docker push everettcaleb/snowflake:$TAG_VERSION
   docker push everettcaleb/snowflake:$TAG_W_MINOR
   docker push everettcaleb/snowflake:$TAG_MAJOR
   docker push everettcaleb/snowflake

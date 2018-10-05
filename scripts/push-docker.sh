67be58d239e5acacaf0928e1cab17032212d00d3
Caleb Everett <everettcaleb95@gmail.com>
2018-10-05 16:46:04 -0400

    Added more tags for docker push

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 if [[ $TRAVIS_TAG ]]; then
+  TAG_W_MINOR=`echo -n $TRAVIS_TAG | sed 's/\.[0-9]*$//g'`
+  TAG_MAJOR=`echo -n $TRAVIS_TAG | sed 's/\.[0-9]*\.[0-9]*$//g'`
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker tag everettcaleb/snowflake everettcaleb/snowflake:$TRAVIS_TAG
+  docker tag everettcaleb/snowflake everettcaleb/snowflake:$TAG_W_MINOR
+  docker tag everettcaleb/snowflake everettcaleb/snowflake:$TAG_MAJOR
   docker push everettcaleb/snowflake:$TRAVIS_TAG
+  docker push everettcaleb/snowflake:$TAG_W_MINOR
+  docker push everettcaleb/snowflake:$TAG_MAJOR
   docker push everettcaleb/snowflake
 fi

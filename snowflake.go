a2ca303786605b50c09f4a3c79f88244a8cea962
Caleb Everett <everettcaleb95@gmail.com>
2019-01-28 08:42:40 -0500

    Added some more tests and logic tests

--- snowflake.go
+++ snowflake.go
@@ -1,13 +1,23 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-const bits41 uint64 = 2199023255551
-const bits10 uint64 = 1023
-const bits12Mod uint64 = 4096
+const (
+	bits10 uint64 = (1 << 10) - 1
+	bits12 uint64 = (1 << 12) - 1
+	bits41 uint64 = (1 << 41) - 1
+	bit12  uint64 = (1 << 12)
+)
+
+var now = func() time.Time {
+	return time.Now()
+}
 
 func msTimestamp() uint64 {
-	return uint64(time.Now().UnixNano()) / uint64(time.Millisecond)
+	return uint64(now().UnixNano() / 1e6)
 }
 
 type snowflakeGenerator struct {
@@ -17,14 +27,33 @@ type snowflakeGenerator struct {
 	epoch         uint64
 }
 
-// MakeSnowflakeGenerator creates a snowflake ID generator instance that tracks generator state
+type snowflakeID struct {
+	id        uint64
+	timestamp uint64
+	machineID uint64
+	counter   uint64
+}
+
+// makeSnowflakeGenerator creates a snowflake ID generator instance that tracks generator state
 func makeSnowflakeGenerator(machineID uint64, epoch uint64) *snowflakeGenerator {
 	return &snowflakeGenerator{
-		counter:       0,
-		lastTimestamp: 0,
-		machineID:     machineID,
-		epoch:         epoch,
+		machineID: machineID,
+		epoch:     epoch,
 	}
+}
+
+func splitSnowflakeID(id uint64) *snowflakeID {
+	sid := &snowflakeID{id: id}
+
+	sid.counter = id & bits12
+	id = id &^ bits12 >> 12
+
+	sid.machineID = id & bits10
+	id = id &^ bits10 >> 10
+
+	sid.timestamp = id
+
+	return sid
 }
 
 // NextID generates the next ID in the sequence
@@ -36,7 +65,7 @@ func (s *snowflakeGenerator) NextID() uint64 {
 	}
 
 	if timestamp == s.lastTimestamp {
-		s.counter = (s.counter + 1) % bits12Mod
+		s.counter = (s.counter + 1) % bit12
 		if s.counter == 0 {
 			for timestamp <= s.lastTimestamp {
 				timestamp = msTimestamp()
@@ -47,5 +76,9 @@ func (s *snowflakeGenerator) NextID() uint64 {
 	}
 
 	s.lastTimestamp = timestamp
-	return (((timestamp - s.epoch) & bits41) << 22) + s.machineID + s.counter
+	td := timestamp - s.epoch
+	if td >= bits41 {
+		panic(fmt.Errorf("Timestamp epoch delta exceeded 41 bits"))
+	}
+	return ((td & bits41) << 22) + ((s.machineID & bits10) << 12) + s.counter
 }
---
+++ snowflake_test.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func fixedNow(s string) func() time.Time {
+	return func() time.Time {
+		t, err := time.Parse(time.RFC3339, s)
+		if err != nil {
+			panic(err)
+		}
+		return t
+	}
+}
+
+func incrementingNow(s string) func() time.Time {
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		panic(err)
+	}
+	return func() time.Time {
+		t = t.Add(time.Microsecond)
+		return t
+	}
+}
+
+func TestMSTimestamp(t *testing.T) {
+	// fmt.Println(msTimestamp())
+	now = fixedNow("2018-07-25T01:14:00Z")
+	tsms := uint64(now().Unix() * 1000)
+	v := msTimestamp()
+	if tsms != msTimestamp() {
+		t.Error("Expected msTimestamp() ->", tsms, "got", v)
+	}
+
+	tsns := uint64(now().UnixNano() / 1e6)
+	v = msTimestamp()
+	if tsns != msTimestamp() {
+		t.Error("Expected msTimestamp() ->", tsns, "got", v)
+	}
+}
+
+func TestSplitSnowflakeID(t *testing.T) {
+	// epoch, err := time.Parse(time.RFC3339, "2016-01-01T00:00:00Z")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	/*sid := */
+	splitSnowflakeID(3044420458909792707)
+	// fmt.Println("Original ID:", sid.id)
+	// fmt.Println("Timestamp (ms):", sid.timestamp)
+	// fmt.Println("Machine ID:", sid.machineID)
+	// fmt.Println("Counter:", sid.counter)
+	// fmt.Println()
+
+	// realTS := epoch.Add(time.Duration(sid.timestamp) * time.Millisecond)
+	// fmt.Println("Real Timestamp (unix):", realTS.Unix())
+}
+
+func TestNextID(t *testing.T) {
+	epoch, err := time.Parse(time.RFC3339, "2016-01-01T00:00:00Z")
+	if err != nil {
+		panic(err)
+	}
+
+	now = fixedNow("2039-01-01T00:00:00Z")
+
+	sg := makeSnowflakeGenerator(1023, uint64(epoch.UnixNano()/1e6))
+
+	ids := make([]uint64, 2500)
+	for i := 0; i < 2500; i++ {
+		ids[i] = sg.NextID()
+	}
+
+	sids := make([]*snowflakeID, 2500)
+	for i, id := range ids {
+		sids[i] = splitSnowflakeID(id)
+	}
+
+	// check to make sure they're ordered
+	for i, id := range ids {
+		if i > 0 && id-1 != ids[i-1] {
+			t.Error("IDs should be sequential!")
+		}
+	}
+
+	// check them all
+	for i, sid := range sids {
+		if i > 0 && sid.counter <= sids[i-1].counter {
+			t.Error("Counter skipped")
+		}
+		if sid.machineID != 1023 {
+			t.Error("Invalid machine ID")
+		}
+		if i > 0 && sid.timestamp != sids[i-1].timestamp {
+			t.Error("Time issue")
+		}
+	}
+
+	// check the format of a few
+
+	// TEMP: printing out the first 100
+	// for i, id := range ids {
+	// 	fmt.Println(id)
+	// }
+}

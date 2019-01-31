ab611fd6de640db4e87e2a5511c24d5f5c2c55b1
Caleb Everett <everettcaleb95@gmail.com>
2019-01-31 07:41:21 -0500

    Fixed docker push script

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -3,7 +3,7 @@ if [[ $TRAVIS_TAG ]]; then
   TAG_VERSION=$(echo -n $TRAVIS_TAG | sed 's/v//')
   TAG_W_MINOR=$(echo -n $TAG_VERSION | sed 's/\.[0-9]*$//g')
   TAG_MAJOR=$(echo -n $TAG_VERSION | sed 's/\.[0-9]*\.[0-9]*$//g')
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  echo -n "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker tag everettcaleb/snowflake everettcaleb/snowflake:$TAG_VERSION
   docker tag everettcaleb/snowflake everettcaleb/snowflake:$TAG_W_MINOR
   docker tag everettcaleb/snowflake everettcaleb/snowflake:$TAG_MAJOR
